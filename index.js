a086c9e44b5b294892948965ba9b877e51330543
Brian Woodward <brian.woodward@gmail.com>
2016-01-18 20:51:12 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * assemble-redirects <https://github.com/assemble/assemble-redirects>
+ *
+ * Copyright (c) 2016, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
